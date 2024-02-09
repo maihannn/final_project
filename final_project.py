60f4b4bbf5ad1ddf86fd3469142f148f09a999fa
maihannn <maicapalillo15@gmail.com>
2024-02-10 00:16:31 +0800

    done

--- final_project.py
+++ final_project.py
@@ -7,39 +7,60 @@ import datetime
 
 capture = cv2.VideoCapture(0)
 
-face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
-body_cascade = cv2.CascadeClassifier(h cv2.data.haarcascades + "haarcascade_fullbody.xml")
+face_cascade = cv2.CascadeClassifier(
+    cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
+body_cascade = cv2.CascadeClassifier(
+    cv2.data.haarcascades + "haarcascade_fullbody.xml")
 
-recording = True
+detection = False
+detection_time_stopped= None
+timer_starts = False
+SECONDS_TO_RECORD_AFTER_DETECTION = 10
 
-frame_size = (int(capture.get(3)), int (capture.get(4)))
+frame_size = (int(capture.get(3)), int(capture.get(4)))
 fourcc = cv2.VideoWriter_fourcc(*"mp4v")
-out = cv2.VideoWriter ("Captured Video.mp4", fourcc, 20, frame_size)
 
 while True:
     _, frame = capture.read()
 
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
-    bodies = face_cascade.detectMultiScale(gray, 1.3, 5)
-
-    if len(faces) + len(bodies) > 0:
-        recording = True
-    
-    out.write (frame)
+    faces = face_cascade.detectMultiScale(gray, 1.1, 5)
+    bodies = face_cascade.detectMultiScale(gray, 1.1, 5)
 
     for (x, y, width, height) in faces:
-        cv2.rectangle (frame (x, y), (x + width, y + height), (255, 0, 0), 3)
+        cv2.rectangle (frame, (x,y), (x + width, y + height), (255, 0, 0), 2)
     for (x, y, width, height) in bodies:
-        cv2.rectangle (frame (x, y), (x + width, y + height), (255, 0, 0), 3)
+        cv2.rectangle (frame, (x,y), (x + width, y + height), (0, 255, 0), 2)
+
+    if len(faces) + len(bodies) > 0:
+        if detection:
+            timer_starts = False
+        else:
+            detection = True
+            current_time = datetime.datetime.now().strftime("%d-%m-%Y-%H-%M-%S")
+            out = cv2.VideoWriter(
+                f"{current_time}.mp4", fourcc, 20, frame_size)
+            print("Start Recording!")
+    elif detection:
+        if timer_starts:
+            if time.time() - detection_time_stopped >= SECONDS_TO_RECORD_AFTER_DETECTION:
+                detection = False
+                timer_starts = False
+                out.release()
+                print('Stop Recording!')
+        else:
+            timer_starts= True
+            detection_time_stopped = time.time()
+
+    if detection:
+        out.write(frame)
     
- 
-    
-    cv2.imshow ("Camera", frame)
+    cv2.imshow("Camera", frame)
 
     if cv2.waitKey(1) == ord('h'):
-        break 
+        break
 
+out.release()
 capture.release()
 cv2.destroyAllWindows()
 
